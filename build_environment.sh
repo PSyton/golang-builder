161704556c20f26e48b99f6d9a6cd3ddefabcd8f
Alex Peters <ap@optiopay.com>
2016-06-03 23:47:07 +0200

    x

--- build_environment.sh
+++ build_environment.sh
@@ -2,7 +2,7 @@
 
 tagName=$1
 
-if ( find /src -maxdepth 0 -empty | read v );
+if ( find /src -maxdepth 0 | read v );
 then
   echo "Error: Must mount Go source code into /src directory"
   exit 990
